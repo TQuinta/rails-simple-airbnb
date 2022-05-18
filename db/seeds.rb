9db288fe1ab6c4f077df71b17a97340596de2394
Tomas Quintanilha <tquinta@alumni.ie.edu>
2022-05-18 16:34:34 +0200

    basic crud completed, styling next

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'creating your flats'
+puts 'thinking'
+10.times do
+  flat = Flat.new(
+    name: Faker::Beer.name,
+    description: Faker::Fantasy::Tolkien.poem,
+    address: Faker::Address.full_address,
+    number_of_guests: rand(1..10),
+    price_per_night: rand(50...500)
+  )
+  puts "creating flat - #{flat.name}"
+  flat.save!
+end
+
+puts 'compeleted the creation of flats'
